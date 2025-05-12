69078e86fe1755ea1401725a74b8023dda6a4e5c
Gabriel Karing <gabhenkar@gmail.com>
2025-05-12 00:38:03 -0300

    Commit

--- backend/database.py
+++ backend/database.py
@@ -5,6 +5,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",  # Endereço do servidor de banco de dados
         user="root",  # Usuário do banco de dados
-        password="GA%0804&ka",  # Senha do banco de dados
+        password="xxxxxxxxx",  # Senha do banco de dados
         database="sistema_pedidos"  # Nome do banco de dados
     )
